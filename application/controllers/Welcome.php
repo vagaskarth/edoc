a3916812e2cde0adc73498476eb83b3cb714ce3e
vagaskarth <vamq14@gmail.com>
2016-12-29 20:12:49 +0800

    fixed typo
    fixed typo

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,7 +21,7 @@ class Welcome extends CI_Controller {
 
 	public function __construct(){
 		parent::__construct();
-		//$this->load->model('Physician_model');
+		$this->load->model('Physician_model');
 		 // $this->load->model('user','',TRUE);
 		 $this->load->library('session');
 	}
